80505ec344ea5db1c4a74c3204487dde8d449327
Harini06-kr <krharini463@gmail.com>
2022-05-15 20:02:36 +0530

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//l
+import './App.css';
+import Basic from "./acomponents/abuild"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Amma </h1>
+      <Basic />
+    </div>
+  );
+}
+
+export default App;
